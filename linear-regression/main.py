6734b3b22b7e804ceff97c3ff20c90e3c2b2ef89
Asim Abdalla <asimabdalla99@gmail.com>
2024-04-22 13:29:17 +0000

    modify linear regression code structure

--- linear-regression/main.py
+++ linear-regression/main.py
@@ -1,19 +1,11 @@
 from data import generate_data
 from model import LinearRegression
-from utils import *
+
 
 Xtrain, ytrain = generate_data()
-model = LinearRegression(Xtrain, ytrain)
+model = LinearRegression()
 
-losses = []
-num_epochs = 10
-for epoch in range(num_epochs):
-    ypred = model.forward_pass()
-    loss = mean_squared_error(ytrain, ypred)
-    grads = model.backward_pass()
-    model.update_param()
+model.fit(Xtrain, ytrain)
 
-    losses.append(loss)
-    print(f"\nEpoch {epoch}, loss {loss}")
 
-print(losses)
+
--- linear-regression/model.py
+++ linear-regression/model.py
@@ -1,21 +1,32 @@
 import numpy as np
+from utils import mean_squared_error
 
 class LinearRegression:
-    def __init__(self, X, y):
-        self.X = X
-        self.y = y
-        _, self.D = self.X.shape
-        self.theta = np.zeros((self.D,1))
-    
-    def forward_pass(self):
-        return np.dot(self.X, self.theta)
+    def __init__(self):
+        self.theta = None
         
-    def backward_pass(self):
-        yhat = self.forward_pass()
-        dtheta =  -2 * np.dot(self.X.T, (self.y - yhat))
+    def forward_pass(self, X):
+        return np.dot(X, self.theta)
+        
+    def backward_pass(self, X, y):
+        yhat = self.forward_pass(X)
+        dtheta =  -2 * np.dot(X.T, (y - yhat))
         return dtheta
     
-    def update_param(self, step_size=0.1):
-        grads = self.backward_pass()
-        self.theta = self.theta - step_size * grads
+    def update_param(self, grads, step_size=0.1):
+        self.theta -= step_size * grads
+        
+    def fit(self, Xtrain, ytrain, n_epochs=10):
+        _, self.D = Xtrain.shape
+        self.theta = np.zeros((self.D,1))
+        losses = []
+    
+        for epoch in range(n_epochs):
+            ypred = self.forward_pass(Xtrain)
+            loss = mean_squared_error(ytrain, ypred)
+            grads = self.backward_pass(Xtrain, ytrain)
+            self.update_param(grads)
+
+            print(f"\nEpoch {epoch}, loss {loss}")
+
         
